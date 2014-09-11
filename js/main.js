fe6652f0063b27ca733ab80795eb76ef438a8ad7
Di Wu <diwuusa@gmail.com>
2014-09-11 17:37:58 -0500

    revise1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 console.log("nihao");
 
-$document.ready.（function(){
+$(document).ready(function() {
   $('.row1').on('sumbmit', '.infor', function(){
     var ditxt = $(this).val();
     $('.row1').append(ditxt);
   })
-}）；
+});
